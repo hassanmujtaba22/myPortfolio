3c99f4899282a1ad2fc947c5bad100c7671bb602
hassanmujtaba <hassan03203406130@gmail.com>
2020-07-18 15:39:17 +0500

    header created

---
+++ src/components/header/Header.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import "./Header.modules.css"
+function Header() {
+    return (
+        <>
+            <header className="header">
+                <a href="/">
+                    <img className="logo" src="/images/logo.png" />
+                </a>
+                <input className="menu-btn" type="checkbox" id="menu-btn" />
+                <label className="menu-icon" htmlFor="menu-btn">
+                    <span className="navicon"></span>
+                </label>
+                <ul className="menu">
+                    <li>
+                        <a href="#skills">Skills</a>
+                    </li>
+                    <li>
+                        <a href="#projects">Projects</a>
+                    </li>
+                    <li>
+                        <a href="#services">Services</a>
+                    </li>
+                    <li>
+                        <a href="#achievements">Achievements</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact Me</a>
+                    </li>
+                </ul>
+            </header>
+        </>
+    )
+}
+
+export default Header
---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <>
+            
+            <img src="./images/homeicon.svg"/>
+        </>
+    )
+}
+
+export default Home
